949c971faa1d3a42f1a8c9755503a704de67c968
Your ikhsan28 <muhi8797@gmail.com>
2026-09-25 23:53:26 +0800

    Update aplikasi

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "apkpenghitungpengeluaran.github.io",
+  base: "/apkpenghitungpengeluaran.github.io/",
 });
